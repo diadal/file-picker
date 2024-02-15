230a578623fbe41bdc073acb3e271b204c6bb504
Diadal <info@diadal.com.ng>
2024-02-15 13:22:44 +0100

    update

--- src/FileSelector.vue
+++ src/FileSelector.vue
@@ -1,61 +1,45 @@
-<template>
-<div>
- <input
-    style="display: none"
-    type="file"
-    ref="inputRef"
-    :multiple="allowMultiple"
-    :accept="accept"
-    @change="updateFiles"
-  />
-  <slot :openDialog="openDialog" />
+<script setup lang="ts">
+import { provide, ref } from "vue";
 
-</div>
- 
-</template>
+interface Pros {
+  modelValue: string[];
+  allowMultiple: Boolean | undefined;
+  accept: string | undefined;
+}
 
-<script lang="ts">
-import { provide, ref, defineComponent } from 'vue';
-export default defineComponent({
-  emits: ['update:modelValue'],
-  props: {
-    modelValue: {
-      type: Array,
-      required: true,
-    },
-    allowMultiple: {
-      type: Boolean,
-      default: true,
-    },
-    accept: {
-      type: String,
-      default: undefined,
-    },
-  },
-  setup(props, { emit }) {
-    const inputRef = ref<HTMLInputElement>(<HTMLInputElement>(<unknown>null));
-    const openDialog = () => {
-      inputRef.value.click();
-    };
+// emits: ["update:modelValue"];
 
-    provide('openDialog', openDialog);
+const emit = defineEmits(["update:modelValue"]);
+const props = defineProps<Pros>();
+const inputRef = ref<HTMLInputElement>(<HTMLInputElement>(<unknown>null));
+const openDialog = () => {
+  inputRef.value.click();
+};
 
-    provide('addFiles', (files: File[]) => {
-      emit('update:modelValue', [...props.modelValue, ...files]);
-    });
+provide("openDialog", openDialog);
 
-    const updateFiles = () => {
-      if (inputRef.value.files?.length) {
-        const files = <File[]>(<unknown>inputRef.value.files);
-        emit('update:modelValue', [...props.modelValue, ...files]);
-      }
-    };
-
-    return {
-      inputRef,
-      openDialog,
-      updateFiles,
-    };
-  },
+provide("addFiles", (files: File[]) => {
+  emit("update:modelValue", [...props.modelValue, ...files]);
 });
+
+const updateFiles = () => {
+  if (inputRef.value.files?.length) {
+    const files = <File[]>(<unknown>inputRef.value.files);
+    emit("update:modelValue", [...props.modelValue, ...files]);
+  }
+};
 </script>
+
+<template>
+  <div>
+    <input
+      style="display: none"
+      type="file"
+      ref="inputRef"
+      :multiple="allowMultiple"
+      :accept="accept"
+      @change="updateFiles"
+    />
+    <slot :openDialog="openDialog" />
+  </div>
+</template>
--- src/PickerButton.vue
+++ src/PickerButton.vue
@@ -1,17 +1,11 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+const openDialog = <() => void>inject("openDialog");
+</script>
+
 <template>
   <div v-bind="$attrs" @click="openDialog" class="col-2">
     <slot />
   </div>
 </template>
-<script>
-import { inject } from 'vue';
-export default {
-  setup() {
-    const openDialog = inject('openDialog');
-
-    return {
-      openDialog,
-    };
-  },
-};
-</script>
